d6f874f280a0fdca688b60460a077cbe8b8cad4d
niyamotpurtravelclub <141675766+niyamotpurtravelclub@users.noreply.github.com>
2023-08-12 00:35:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const passwordForm = document.getElementById("password-form");
+    const content = document.getElementById("content");
+  
+    const submitButton = document.getElementById("submit-button");
+    submitButton.addEventListener("click", function() {
+      const passwordInput = document.getElementById("password-input");
+      const enteredPassword = passwordInput.value;
+  
+      // Replace 'yourpassword' with the actual password
+      if (enteredPassword === "yourpassword") {
+        passwordForm.style.display = "none";
+        content.style.display = "block";
+      } else {
+        alert("Incorrect password. Please try again.");
+      }
+    });
+  });
+  
